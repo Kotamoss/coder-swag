55bf7d0521416852f0b27429c1615ac95f3ad749
Kotamoss <storyofko@gmail.com>
2018-05-28 11:38:00 -0400

    created new groups

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by R$KY on 5/28/18.
+//  Copyright © 2018 Dakotah Moss. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
