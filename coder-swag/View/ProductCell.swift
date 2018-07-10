b4aa803e4a5a6b615bee031c39c15fccb5590f79
Kotamoss <storyofko@gmail.com>
2018-07-09 21:29:59 -0400

    Finished building collection view and linking IBOutlets for productimage and productprice.

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by R$KY on 7/9/18.
+//  Copyright © 2018 Dakotah Moss. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+}
